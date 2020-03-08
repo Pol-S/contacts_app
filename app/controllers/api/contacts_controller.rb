316902292797bb29c5eac37e9cb5e2f91a296494
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-08 09:52:46 -0500

    Got the post action to work.

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,16 +1,23 @@
 class Api::ContactsController < ApplicationController
+  def index
+    @all_dudes = Contact.all
+    render "index.json.jb"
+  end
+
   def show
     @contact = Contact.find_by(id: params[:id])
-
     render "show.json.jb"
   end
 
-  def index
-    @all_dudes = Contact.all
-    # index = 0
-    # while index < @all_dudes.length
-    #   @current_trick = @all_dudes[index]
-    render "index.json.jb"
-    # end
+  def post
+    @contact = Contact.new({
+      first_name: "Kyle",
+      last_name: "Ross",
+      email: "kross@example.com",
+      phone_number: "456 789-1234",
+    })
+    @contact.save
+
+    render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/contacts" => "contacts#index"
     get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#post"
   end
 end
