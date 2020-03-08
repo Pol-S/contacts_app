73ab0905dbe39ee4315103c715a675b5cd6195b5
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-08 10:20:35 -0500

    Got the update action to work, even with partial data

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     get "/contacts" => "contacts#index"
     get "/contacts/:id" => "contacts#show"
     post "/contacts" => "contacts#post"
+    patch "/contacts/:id" => "contacts#update"
   end
 end
