cf044fd99f2fd54601cd8bc65efc511d85c4f278
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-08 11:37:32 -0500

    Cleaned up them model and viewer to display full names and updated times

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%B %e, %Y, %l:%M %P")
+  end
+
+  def full_name
+    full_name = "#{first_name} #{last_name}"
+  end
 end
