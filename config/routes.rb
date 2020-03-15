2928c3dc94cc0ce9dff48c635e35365ac266c99b
Pol Subanajouy <plsbnjy@gmail.com>
2020-03-15 11:57:16 -0500

    Added user model, session create, sign in route and loge in route via cheat sheet

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    post "/users" => "users#create" #sometimes called sign-in
+
+    post "/sessions" => "sessions#create" #sometimes called login
+
     get "/contacts" => "contacts#index"
     get "/contacts/:id" => "contacts#show"
     post "/contacts" => "contacts#post"
